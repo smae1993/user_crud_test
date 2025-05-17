ec0845a8c58d443d6d99b268408a585c24470b48
smae1993 <smae1993@gmail.com>
2025-05-17 11:08:15 +0330

    create user data and factory and seed

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,10 +24,11 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
+            'name' => fake()->firstName(),
+            'family' => fake()->lastName(),
+            'mobile' => fake()->unique()->phoneNumber(),
+            // 'email_verified_at' => now(),
+            // 'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
         ];
     }
